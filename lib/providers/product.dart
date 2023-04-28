a82ab6e96022fd5145f56d977d0e93b8d30a0204
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-04-28 19:27:09 +0700

    обновление  статуса товара - добавить/удалить из избанного PATCH запрос

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,9 +19,40 @@ class Product with ChangeNotifier {
     required this.imageUrl,
      this.isFavorite = false,
   });
+   void _setFavoriteValue(bool newValue){
+     isFavorite = newValue;
+     notifyListeners();
+   }
 
-   void toggleFavoriteStatus(){
+
+   /*Добавить товар в избранное,
+   * убрать из избранного
+   * Если добавить в избранное невозможно,
+   * то происходит возврат в начальное состояние*/
+   Future<void> toggleFavoriteStatus() async {
+     final oldStatus = isFavorite;
      isFavorite = !isFavorite;
      notifyListeners(); //обновить состояние
+
+     final Uri url = Uri.parse(
+         'https://shopapp-67ba1-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json');
+
+     /*изменяем статус избранного на сервере
+     * PATCH запросом*/
+     try{
+       final responce = await http.patch(url, body: json.encode({
+         'isFavorite': isFavorite,
+       }));
+
+       if(responce.statusCode >= 400){
+         _setFavoriteValue(oldStatus);
+       }
+     }
+     catch(error){
+      //откат в начальное состояние
+       _setFavoriteValue(oldStatus);
+     }
+
+
    }
 }
